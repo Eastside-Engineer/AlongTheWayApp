c3c07f6ac62ecef35a4cb54f3bfa468b0e9452a8
Cynthia <cturner@ltu.edu>
2019-03-07 16:42:57 -0500

    initial commit

---
+++ src/main/java/finalproject/alongtheway/AlongTheWayApplication.java
@@ -0,0 +1,13 @@
+package finalproject.alongtheway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlongTheWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlongTheWayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/finalproject/alongtheway/AlongTheWayController.java
@@ -0,0 +1,8 @@
+package finalproject.alongtheway;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AlongTheWayController {
+
+}
