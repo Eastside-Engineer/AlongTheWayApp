f8059fe9fd77ca53c438f8a393d996446a6d08b6
Maltheos <krzysztof.grabka@gmail.com>
2019-03-08 14:33:53 -0500

    api service test 1

--- src/main/java/finalproject/alongtheway/AlongTheWayController.java
+++ src/main/java/finalproject/alongtheway/AlongTheWayController.java
@@ -1,15 +1,20 @@
 package finalproject.alongtheway;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import finalproject.alongtheway.model.Businesses;
+
 @Controller
 public class AlongTheWayController {
 
-
-
+	@Autowired
+	private BusinessSearchApiService businessSearchService;
+	
 	@RequestMapping("/")
 	public ModelAndView list() {
 
@@ -17,5 +22,11 @@ public class AlongTheWayController {
 
 	}
 
-
+	@RequestMapping("/results")
+	public ModelAndView results() { 
+		List<Businesses> results;
+		results = businessSearchService.getAllResultsByLocation();
+		ModelAndView mav = new ModelAndView("results", "results", results);
+		return mav;
+	}
 }
--- src/main/java/finalproject/alongtheway/BusinessSearchApiService.java
+++ src/main/java/finalproject/alongtheway/BusinessSearchApiService.java
@@ -1,10 +1,18 @@
 package finalproject.alongtheway;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import finalproject.alongtheway.model.BusinessSearchResponse;
+import finalproject.alongtheway.model.Businesses;
 
 @Component
 public class BusinessSearchApiService {
@@ -23,6 +31,23 @@ public class BusinessSearchApiService {
 			return execution.execute(request, body);
 		};
 		restTemplateWithUserAgent = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
+	}
+	
+	// api key values stored in application.properties
+	@Value("${myapi.key}")
+	private String apikey;
+		
+	public List<Businesses> getAllResultsByLocation() {
+		String location = "Detroit";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + apikey);
+		String url = "https://api.yelp.com/v3/businesses/search?location=" + location;
+		BusinessSearchResponse apiResponse = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), BusinessSearchResponse.class).getBody();
+		return apiResponse.getBusinesses();
+	}
+	
+	public List<Businesses> getResultsByCoord(){
+		return null;
 	}
 	
 }
